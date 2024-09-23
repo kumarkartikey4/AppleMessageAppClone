7841c96182bfb961574ffcce60a29f5c7f996c3d
Kartikey Kumar <k2kartikey.pathak@gmail.com>
2024-09-24 00:55:07 +0530

    add section component.

---
+++ src/components/Section.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import {Image, FlatList, StyleSheet, View, Text} from 'react-native';
+import IconButton from './IconButton';
+
+const Section = ({navigation, data}: any) => {
+  return (
+    <FlatList
+      style={styles.container}
+      data={data}
+      renderItem={({item}) => (
+        <View style={styles.box}>
+          <View style={styles.boxItemLeft}>
+            <Image
+              source={{uri: item.icon}}
+              style={{
+                marginRight: 10,
+                width: 20,
+                height: 20,
+                marginHorizontal: 2,
+              }}
+            />
+            <Text style={{fontSize: 14}}>{item.name}</Text>
+          </View>
+          <View style={styles.boxItemRight}>
+            <Text style={{marginRight: 5}}>{item.count}</Text>
+            <IconButton
+              title=""
+              iconUrl="https://img.icons8.com/ios/50/forward--v1.png"
+              onPress={() =>
+                navigation.navigate('list', {key: item.key})
+              }></IconButton>
+          </View>
+        </View>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+    backgroundColor: 'white',
+    borderRadius: 15,
+    shadowColor: 'black',
+    shadowOpacity: 0.3,
+  },
+  box: {
+    margin: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    width: '100%',
+    height: 45,
+    flex: 1,
+    flexDirection: 'row',
+    // backgroundColor: 'lightblue',
+    // justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  boxItemLeft: {
+    margin: 1,
+    // paddingLeft: 10,
+    paddingRight: 10,
+    // padding: 10,
+    width: '100%',
+    height: 30,
+    flex: 1,
+    flexDirection: 'row',
+    // backgroundColor: 'grey',
+    // justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  boxItemRight: {
+    // margin: 1,
+    // paddingLeft: 10,
+    paddingRight: 10,
+    // padding: 10,
+    // width: '100%',
+    height: 30,
+    flex: 0,
+    flexDirection: 'row',
+    // backgroundColor: 'grey',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+});
+
+export default Section;
