814944665fad8321678a7c777951ec39d27c4dd9
Kartikey Kumar <k2kartikey.pathak@gmail.com>
2024-09-24 23:40:46 +0530

    add message detail page

---
+++ src/pages/MessageDetails.tsx
@@ -0,0 +1,8 @@
+import {View, Text} from 'react-native'
+
+const MessageDetails = (props: any) => {
+    return (<View>
+        <Text>Details Screen</Text>
+      </View>)
+};
+export default MessageDetails;
