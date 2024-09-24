97ec729ffdb1aa585405341964b4c53c66e7bae1
Kartikey Kumar <k2kartikey.pathak@gmail.com>
2024-09-24 23:38:56 +0530

    add loading when app starts.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
@@ -8,8 +8,24 @@ import 'react-native-gesture-handler';
 
 import Home from './src/pages/Home';
 import MessageList from './src/pages/MessagesList';
+import MessageDetails from './src/pages/MessageDetails';
+import LoadingScreen from './src/components/Loading';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    // Simulate loading process
+    const timer = setTimeout(() => {
+      setIsLoading(false); // Set loading to false after 3 seconds
+    }, 3000);
+
+    return () => clearTimeout(timer); // Cleanup timer on unmount
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -21,6 +37,15 @@ const App = () => {
         <Stack.Screen
           name="list"
           component={MessageList}
+          options={{
+            title: '',
+            headerStyle: {backgroundColor: '#fff'},
+            headerTintColor: '#000',
+          }}
+        />
+        <Stack.Screen
+          name="detail"
+          component={MessageDetails}
           options={{
             title: '',
             headerStyle: {backgroundColor: '#fff'},
